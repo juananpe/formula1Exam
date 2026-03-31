bdeb67e3d0a0a12e04bce9138dc3664cc904a2d0
Juanan Pereira <juanan.pereira@ehu.eus>
2026-03-31 15:56:59 +0200

    Fix TCP server port to use hardcoded value instead of constant

--- src/main/java/eus/ehu/data_access/H2WebLauncher.java
+++ src/main/java/eus/ehu/data_access/H2WebLauncher.java
@@ -22,8 +22,8 @@ public class H2WebLauncher {
             System.out.println("Press Ctrl+C to stop the server...");
             
             // Optional: If you want to also start a TCP server for other applications to connect
-            Server tcpServer = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", TCP_PORT).start();
-            System.out.println("H2 TCP server started on port " + TCP_PORT + ".");
+            Server tcpServer = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
+            System.out.println("H2 TCP server started on port 9092.");
             
         } catch (Exception e) {
             System.out.println("Error starting H2 server: " + e.getMessage());
